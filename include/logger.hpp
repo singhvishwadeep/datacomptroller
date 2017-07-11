5df48df0dbd7b4ec612f09d592fa16d5f5c8eb6c
Vishwadeep Singh <vsdpsingh@gmail.com>
2017-07-11 07:35:32 +0530

    Update logger.hpp

--- include/logger.hpp
+++ include/logger.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <fstream>
 #include <thread>
 #include <mutex>
 #include <chrono>
@@ -26,7 +27,7 @@ using namespace std;
 class logger {
 	int port;
 	int rotation;
-	int kill1, kill2, kill3, kill4;
+	int kill1, kill2, kill3, kill4, kill5;
 	string tag;
 	string filter;
 	string nofilter;
@@ -54,8 +55,7 @@ public:
 			filter(gfilter),nofilter(gnofilter),
 			outmode(goutmode), outnames(goutnames), outports(goutports)  {
 		cout << "Called Constructor for tag=" << gtag << " with port=" << gport << endl;
-		killall = 0;
-		killsignal = 0;
+		kill1 = kill2 = kill3 = kill4 = kill5 = 0;
 		// create listen handle
 		cout << "opening reading socket..." << endl;
 		createsocketlistener();
@@ -112,15 +112,15 @@ public:
 		cout << "socket listener created properly" << endl;
 		return;
 	}
-	
+
 	void openwritehandles () {
 		int i = 0;
 		for (auto it = outmode.begin(); it != outmode.end(); it++) {
-			if (it == 0) {
+			if (*it == 0) {
 				// file mode
 				ofstream myfile;
-				myfile.open (outnames[i]);
-				writehandle.push_back(myfile);
+				myfile.open(outnames[i]);
+				writehandle.push_back(move(myfile));
 			} else {
 				// socket mode
 				// :TODO
@@ -130,7 +130,7 @@ public:
 		cout << "write handles are opened properly" << endl;
 	}
 
-	int accept_client(int s, int timeout) {
+	static int accept_client(int s, int timeout) {
 	   int iResult;
 	   struct timeval tv;
 	   fd_set rfds;
@@ -151,22 +151,22 @@ public:
 	   }
 	   return 0;
 	}
-	
+
 	static void listener_function_parent (logger &log) {
 		// listening for incoming messages
 		struct sockaddr_in serv_addr;
-		listenhandle = socket(AF_INET, SOCK_STREAM, 0);
+		log.listenhandle = socket(AF_INET, SOCK_STREAM, 0);
 		// creating a socket for sever
-		if (listenhandle < 0) {
+		if (log.listenhandle < 0) {
 			cerr << "ERROR opening socket";
 			exit(1);
 		}
 		bzero((char *) &serv_addr, sizeof(serv_addr));
 		serv_addr.sin_family = AF_INET;
 		serv_addr.sin_addr.s_addr = INADDR_ANY;
-		serv_addr.sin_port = htons(port);
+		serv_addr.sin_port = htons(log.port);
 		/* Now bind the host address using bind() call.*/
-		if (bind(listenhandle, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		if (bind(log.listenhandle, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
 			perror("ERROR on binding");
 			exit(1);
 		}
@@ -174,14 +174,14 @@ public:
 		tv.tv_sec = LISTEN_TIMEOUT;  // in seconds
 		tv.tv_usec = 0;
 		// setting the time out of recv call
-		setsockopt(listenhandle, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv,sizeof(struct timeval));
-		listen(listenhandle,MAX_LISTEN);
+		setsockopt(log.listenhandle, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv,sizeof(struct timeval));
+		listen(log.listenhandle,MAX_LISTEN);
 		cout << "socket listener created properly" << endl;
-		vector <thread *> allth;
+		vector <thread> allth;
 		while (1) {
 			printf ("listening for new connection client...\n");
 			// newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &addrlen);
-			newsockfd = accept_client(sockfd, 30);
+			int newsockfd = accept_client(log.listenhandle, 30);
 			printf("Recieved someone...\n");
 			if (newsockfd <= 0) {
 				perror("ERROR on accept");
@@ -192,14 +192,15 @@ public:
 				break;
 			}
 			/* Create child thread */
-			thread t1 = thread(listener_function, ref(log), newsockfd);
-			allth.push_back(&t1);
+//			thread t1 = thread(listener_function, ref(log), newsockfd);
+			allth.emplace_back(&listener_function, std::ref(log), newsockfd);
+//			allth.push_back(move(t1));
 		}
-		for (auto it = allth.begin(); it != allth.end(); it++) {
-			it->join();
+		for (auto& it = allth.begin(); it != allth.end(); it++) {
+			it.join();
 		}
 	}
-	
+
 	static void listener_function (logger &log, int sock) {
 		struct timeval tv;
 		tv.tv_sec = LISTEN_TIMEOUT;  // in seconds
@@ -220,7 +221,7 @@ public:
 			cout << "got the message =" << buffer << endl;
 			if (n <= 0) {
 				cerr << "ERROR reading from socket" << endl;
-				continue
+				continue;
 			}
 			// insert into message vector
 			if (string(buffer) == "exit") {
@@ -229,14 +230,14 @@ public:
 			}
 			log.mu_for_message.lock();
 			cout << "pushing the message in incoming_messages" << endl;
-			incoming_messages.push_back(string(buffer));
+			log.incoming_messages.push_back(string(buffer));
 			log.mu_for_message.unlock();
 		}
 		close(sock);
-		kill2 =1;
+		log.kill2 =1;
 		cout << "ending the listener_function thread" << endl;
 	}
-	
+
 	static void collector_function (logger &log) {
 		while (1) {
 			if (log.kill2 == 1) {
@@ -247,50 +248,52 @@ public:
 			this_thread::sleep_for(std::chrono::seconds(1));
 			log.mu_for_message.lock();
 			// move everything to writing_messages
-			if (!incoming_messages.empty()) {
+			if (!log.incoming_messages.empty()) {
 				cout << "inserting the messages inside writing_messages from incoming_messages" << endl;
-				writing_messages.insert(writing_messages.end(), incoming_messages.begin(), incoming_messages.end());
-				incoming_messages.clear();
+				log.writing_messages.insert(log.writing_messages.end(), log.incoming_messages.begin(), log.incoming_messages.end());
+				log.incoming_messages.clear();
 			}
 			log.mu_for_message.unlock();
 		}
-		kill3 = 1;
+		log.kill3 = 1;
 		cout << "ending the collector_function thread" << endl;
 	}
-	
+
 	static void writer_function (logger &log) {
 		while (1) {
-			if (log.killall == 1) {
+			if (log.kill3 == 1) {
 				cout << "killing collector thread" << endl;
 				break;
 			}
 			log.mu_for_message.lock();
 			// write everything to output
 			if (!log.writing_messages.empty()) {
-				for (auto it = log.writing_messages.begin(); it != log.writing_messages.end(); it++) {
+				for (auto& it = log.writing_messages.begin(); it != log.writing_messages.end(); it++) {
 					for (auto it1 = log.writehandle.begin(); it1 != log.writehandle.end(); it1++) {
 						it << it1 << endl;
 					}
 				}
-				writing_messages.clear();
+				log.writing_messages.clear();
 			}
 			log.mu_for_message.unlock();
 		}
+		log.kill4 = 1;
+		cout << "ending the writer_function thread" << endl;
 	}
-	
+
 	static void rotator_function (logger &log) {
 		while (1) {
-			if (log.kill3 == 1) {
+			if (log.kill4 == 1) {
 				cout << "killing rotator_function thread" << endl;
 				break;
 			}
 			cout << "rotator_function thread running.." << endl;
 			this_thread::sleep_for(std::chrono::seconds(2));
 		}
-		kill4 = 1;
+		log.kill4 = 1;
 		cout << "ending the rotator_function thread" << endl;
 	}
-	
+
 	static void killer_function (logger &log) {
 		while (1) {
 			if (log.kill4 == 1) {
@@ -301,14 +304,15 @@ public:
 			this_thread::sleep_for(std::chrono::seconds(2));
 		}
 		for (auto it1 = log.writehandle.begin(); it1 != log.writehandle.end(); it1++) {
-			it.close();
+			it1.close();
 		}
 		// close socket writing
+		log.kill5 = 1;
 		cout << "ending the killer_function thread" << endl;
-		
+
 	}
-	
-	
+
+
 	int getPort () {
 		return port;
 	}
